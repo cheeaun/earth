2d66673ff7d6c18d5815436f0c226e2c8e8561c9
Lim Chee Aun <cheeaun@gmail.com>
2018-06-17 17:57:13 +0800

    Fix uglify not working after upgrade

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import clean from 'postcss-clean';
 
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'assets/app.raw.js',
